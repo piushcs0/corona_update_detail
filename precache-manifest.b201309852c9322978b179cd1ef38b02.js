da0097607554e505cf76d1007d7de0251ef985d8
piush <piush3898@gmail.com>
2020-05-05 13:29:06 +0530

    Updates

---
+++ precache-manifest.b201309852c9322978b179cd1ef38b02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecc791bbd0d06748cd58f8da11c684fc",
+    "url": "/corona_update_detail/index.html"
+  },
+  {
+    "revision": "1752bf79e8bc3c894686",
+    "url": "/corona_update_detail/static/css/main.c5501932.chunk.css"
+  },
+  {
+    "revision": "e3f84ccc1927a4d2a754",
+    "url": "/corona_update_detail/static/js/2.9135bf6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/corona_update_detail/static/js/2.9135bf6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1752bf79e8bc3c894686",
+    "url": "/corona_update_detail/static/js/main.26b4eb13.chunk.js"
+  },
+  {
+    "revision": "12d46126b533564f4b5d",
+    "url": "/corona_update_detail/static/js/runtime-main.8cd89649.js"
+  }
+]);
